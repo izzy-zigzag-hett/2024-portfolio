b49470cf26c58e7e7ecab66139b2e8523e25342e
ihettman <78113281+ihettman@users.noreply.github.com>
2024-01-31 17:26:21 -0500

    initial commit

---
+++ scripts/menu.js
@@ -0,0 +1,4 @@
+function menu_home_popup() {
+    var popup = document.getElementById("home_popup");
+    popup.classList.toggle("show");
+  }
---
+++ scripts/mograph.js
@@ -0,0 +1,29 @@
+var mood_paths = [
+    'images/Flourish/1_awesome_v1.json',
+    'images/Flourish/2_good_v3.json',
+    'images/Flourish/3_okay_v1.json',
+    'images/Flourish/4_bad_v1_FINAL.json',
+    './images/Flourish/5_terrible_v3.json'
+];
+
+var anim_mood_big = bodymovin.loadAnimation({
+    container: document.getElementById('anim_mood_big'),
+    path: './images/Flourish/1_awesome_v1.json',
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    name: "Sunnie Reacts - Awesome",
+}); 
+
+function change_cover(num) {
+    document.getElementById('anim_mood_big').innerHTML = "";
+    anim_mood_big = bodymovin.loadAnimation({
+        container: document.getElementById('anim_mood_big'),
+        path: mood_paths[num - 1],
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        name: "Sunnie Reacts - Awesome",
+    });
+    
+}
